cfcadde09c0869053e46f8437e9787fc5ef2cee6
nandinideshmukh <150786311+nandinideshmukh@users.noreply.github.com>
2024-10-02 16:13:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask,request,jsonify,render_template
 import joblib
 from models.model import predict_crop
 import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def home():
     return render_template('index.html')
